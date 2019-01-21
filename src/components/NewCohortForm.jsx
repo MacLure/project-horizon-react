dc2985c5b4471b19715b13b2429a330432a5f79e
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2019-01-21 14:49:57 -0500

    merging...

--- src/components/NewCohortForm.jsx
+++ src/components/NewCohortForm.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
 class NewCohortForm extends Component {
-  state = { 
+  state = {
     name: '',
     course_type: '',
     start_date: '',
     end_date: '',
    }
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div>
-      New Cohort
-        <form>
+        <form className="container_large">
+          <h2>New Cohort</h2>
           <label for="name">Name</label>
           <input type="text" name="name"></input>
-        
+
           <label for="course_type">Course Type</label>
           <input type="text" name="course_type"></input>
 
@@ -25,10 +25,12 @@ class NewCohortForm extends Component {
           <label for="end_date">End Date</label>
           <input type="date" name="end_date"></input>
 
+          <br/><button type="submit" className="submit">Submit</button>
+
         </form>
       </div>
      );
   }
 }
- 
+
 export default NewCohortForm;
