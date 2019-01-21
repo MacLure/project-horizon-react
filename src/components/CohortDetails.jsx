6cc2f4fd9b68973b5364eb2b2938744c7f76966e
Benjamin <hoppeconnect@gmail.com>
2019-01-21 14:45:09 -0500

    more form styling

--- src/components/CohortDetails.jsx
+++ src/components/CohortDetails.jsx
@@ -7,32 +7,28 @@ const CohortDetails = (props) => {
   const students = props.students
 
   return (
-    <div>
-    {name} {course_type}<br />
-    {start_date} - {end_date}<br />
+    <div className="cohort_card">
 
-    Staff:
-
+    <p>Staff:</p>
     <ul>
     {admins.map(admin => (
       <li key={admin.id}>{admin.first_name} {admin.last_name}</li>
     ))}
     </ul>
 
-    <button>add staff member</button> <br />
+    <button className="submit">add staff member</button> <br />
 
-    Students:
+    <p>Students:</p>
     <ul>
     {students.map(student => (
       <li key={student.id}>{student.first_name} {student.last_name}</li>
     ))}
     </ul>
 
-    <button>add student</button>
-    <button>to students</button> <br />
+    <br/><button className="submit">add student</button>
 
     </div>
   );
 }
- 
+
 export default CohortDetails;
