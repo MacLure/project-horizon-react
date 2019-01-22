edebc7d19e8593f1a6be9f38782356e0f39e88fb
Benjamin <hoppeconnect@gmail.com>
2019-01-22 11:51:07 -0500

    fixed forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
-import Footer from './Footer';
+import React, { Component } from ‘react’;
+import {Link} from “react-router-dom”;
+import Footer from ‘./Footer’;
 
 class Login extends Component {
-  state = {  }
-  render() {
-    return (
-      <div>
-        <div className="container_large">
-          <form method="post" className="container_large">
-          <h2>Welcome Back</h2>
-            <label htmlfor="email">Email</label>
-            <input type="text" name="email"></input>
-            <label htmlfor="password">Password</label>
-            <input type="password" name="password"></input>
+ state = {  }
+ render() {
+   return (
+     <div>
+       <div className=“container_large”>
+         <form method=“post” className=“container_large”>
+         <h2>Welcome Back</h2>
+           <label htmlfor=“email”>Email</label>
+           <input type=“text” name=“email”></input>
+           <label htmlfor=“password”>Password</label>
+           <input type=“password” name=“password”></input>
 
-            <br/><button type="submit" className="submit">Submit</button>
-          </form>
-        </div>
+           <br/><button type=“submit” className=“submit”>Submit</button>
+         </form>
+       </div>
 
-        <Footer/>
+       <Footer/>
 
-      </div>
-      );
-  }
+     </div>
+     );
+ }
 }
 
 export default Login;
--- src/components/NewCohortForm.jsx
+++ src/components/NewCohortForm.jsx
@@ -1,40 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from ‘react’;
 
 class NewCohortForm extends Component {
-  state = {
-    name: '',
-    course_type: '',
-    start_date: '',
-    end_date: '',
-   }
-
-  render() {
-    return (
-      <div>
-        <form className="container_large">
-          <h2>New Cohort</h2>
-          <label htmlfor="name" placeholder="bb8">Name</label>
-          <input type="text" name="name"></input>
-
-          <label htmlfor="course_type">Course Type</label>
-          <select name="course_type">
-            <option value="wdi">Web Development </option>
-            <option value="uxdi">User Expierence</option>
-            <option value="dsi">Data Science</option>
-          </select>
-
-          <label htmlfor="start_date">Start Date</label>
-          <input type="date" name="start_date"></input>
-
-          <label htmlfor="end_date">End Date</label>
-          <input type="date" name="end_date"></input>
-
-          <br/><button type="submit" className="submit">Submit</button>
-
-        </form>
-      </div>
-     );
+ constructor(props) {
+ super(props);
+ this.state = {
+   name: ‘’,
+   course_type: ‘’,
+   start_date: ‘’,
+   end_date: ‘’,
   }
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+
+ }
+
+ handleChange(event) {
+   this.setState({[event.target.name]: event.target.value});
+ }
+
+ handleSubmit(event) {
+   event.preventDefault();
+   console.log(‘new cohort submitted: ’ + this.state.name);
+   event.preventDefault();
+ }
+
+ render() {
+   return (
+     <div>
+     <form className=“container_large” onSubmit={this.handleSubmit}>
+       <h2>New Cohort</h2>
+       <label htmlFor=“name” placeholder=“bb8”>Name</label>
+       <input type=“text” name=“name” value={this.state.name} onChange={this.handleChange} ></input>
+
+       <label htmlFor=“course_type”>Course Type</label>
+       <select name=“course_type” value={this.state.course_type} onChange={this.handleChange} >
+         <option value=“wdi”>Web Development </option>
+         <option value=“uxdi”>User Expierence</option>
+         <option value=“dsi”>Data Science</option>
+       </select>
+
+       <label htmlFor=“start_date”>Start Date</label>
+       <input type=“date” name=“start_date” value={this.state.start_date} onChange={this.handleChange} ></input>
+
+       <label htmlFor=“end_date”>End Date</label>
+       <input type=“date” name=“end_date” value={this.state.end_date} onChange={this.handleChange} ></input>
+
+       <br/><button type=“submit” className=“submit”>Submit</button>
+
+     </form>
+   </div>
+  );
+}
 }
 
 export default NewCohortForm;
