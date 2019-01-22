275b33624c7edcfd56f05d47e1c73d5a6a458633
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2019-01-21 21:29:19 -0500

    submit function for submit student component

--- src/components/NewStudentForm.jsx
+++ src/components/NewStudentForm.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
 class NewStudentForm extends Component {
-  state = {
-    first_name: '',
+  constructor(props) {
+    super(props);
+    this.state = {    first_name: '',
     last_name: '',
     phone: '',
     email: '',
@@ -10,22 +11,37 @@ class NewStudentForm extends Component {
     image_url: ''
    }
 
+   this.handleChange = this.handleChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('new student submitted: ' + this.state);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <form method="post" className="container_large">
+        <form method="post" className="container_large" onSubmit={this.handleSubmit}>
         <h2>New Student</h2>
-          <label for="first_name">First Name</label>
-          <input type="text" name="first_name"></input>
+          <label htmlFor="first_name">First Name</label>
+          <input type="text" name="first_name"  value={this.state.first_name} onChange={this.handleChange}></input>
 
-          <label for="last_name">Last Name</label>
-          <input type="text" name="last_name"></input>
+          <label htmlFor="last_name">Last Name</label>
+          <input type="text" name="last_name"  value={this.state.last_name} onChange={this.handleChange}></input>
 
-          <label for="phone">Phone Number</label>
-          <input type="tel" name="phone"></input>
+          <label htmlFor="phone">Phone Number</label>
+          <input type="tel" name="phone" value={this.state.phone} onChange={this.handleChange}></input>
 
-          <label for="email">Email</label>
-          <input type="text" name="email"></input>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email"  value={this.state.email} onChange={this.handleChange}></input>
 
           <br/><button type="submit" className="submit">Submit</button>
         </form>
