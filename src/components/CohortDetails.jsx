c8357b1eea8fd16cd1c13a52d59aacac41996ec8
Benjamin <hoppeconnect@gmail.com>
2019-01-23 10:27:48 -0500

    merge conflicts

--- src/components/CohortDetails.jsx
+++ src/components/CohortDetails.jsx
@@ -6,7 +6,6 @@ const CohortDetails = (props) => {
  const admins = props.admins
  const students = props.students
 
-<<<<<<< HEAD
   return (
     <div className="cohort_card grid">
       <div className="left">
@@ -30,31 +29,6 @@ const CohortDetails = (props) => {
     </div>
   </div>
   );
-=======
- return (
-   <div className="cohort_card grid">
-     <div className="left">
-       <p>Staff:</p>
-       <ul>
-         {admins.map(admin => (
-           <li key={admin.id}>{admin.first_name} {admin.last_name}</li>
-         ))}
-       </ul>
-       <button className="submit">add staff member</button> <br />
-     </div>
-
-     <div className="right">
-     <p>Students:</p>
-     <ul>
-       {students.map(student => (
-         <li key={student.id}>{student.first_name} {student.last_name}</li>
-       ))}
-     </ul>
-     <button className="submit">add student</button>
-   </div>
- </div>
- );
->>>>>>> react_styles
 }
 
 export default CohortDetails;
--- src/components/NewStudentForm.jsx
+++ src/components/NewStudentForm.jsx
@@ -61,57 +61,6 @@ class NewStudentForm extends Component {
       </div>
      );
   }
-
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-
- }
-
- handleChange(event) {
-   this.setState({[event.target.name]: event.target.value});
- }
-
- handleSubmit(event) {
-   event.preventDefault();
-   console.log('new student submitted: ' + this.state);
-   event.preventDefault();
- }
-
- render() {
-   return (
-     <div>
-       <form method="post" className="container_large" onSubmit={this.handleSubmit}>
-       <h2>New Student</h2>
-
-         <label htmlFor="first_name">First Name</label>
-         <input type="text" name="first_name"  value={this.state.first_name} onChange={this.handleChange}></input>
-
-         <label htmlFor="last_name">Last Name</label>
-         <input type="text" name="last_name"  value={this.state.last_name} onChange={this.handleChange}></input>
-
-         <label htmlFor="phone">Phone Number</label>
-         <input type="tel" name="phone" value={this.state.phone} onChange={this.handleChange}></input>
-
-         <label htmlFor="email">Email</label>
-         <input type="text" name="email"  value={this.state.email} onChange={this.handleChange}></input>
-
-         <label htmlFor="first_name">First Name</label>
-         <input type="text" name="first_name"></input>
-
-         <label htmlFor="last_name">Last Name</label>
-         <input type="text" name="last_name"></input>
-
-         <label htmlFor="phone">Phone Number</label>
-         <input type="tel" name="phone"></input>
-
-         <label htmlFor="email">Email</label>
-         <input type="text" name="email"></input>
-
-         <br/><button type="submit" className="submit">Submit</button>
-       </form>
-     </div>
-    );
- }
 }
 
 export default NewStudentForm;
