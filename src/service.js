75235d73636a416d8a3c159d98adc2386eafaecc
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2019-01-28 15:41:06 -0500

    wpring up loggin

--- src/service.js
+++ src/service.js
@@ -1,12 +1,12 @@
 export const onLogin = (email,password) => {
   return new Promise((resolve,reject)=>{
-    fetch('https://project-horizon-rails.herokuapp.com/admin/admin_token',{
+    fetch(`https://project-horizon-rails.herokuapp.com/admin/admin_token`,
+    {
       method:'POST',
       mode: 'cors',
       headers: {
         "Accept":"application/json",
-        "Content-Type":"application/json"
-      },
+       },
       body:JSON.stringify({ "auth": {"email":email, "password":password} })
     })
     .then(e=>e.ok?resolve(e):reject(e))
