ea759e5a0845afd8cd2fe5bb19159e83f6c7000c
Benjamin <hoppeconnect@gmail.com>
2019-01-31 14:14:21 -0500

    added closing container tag

--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -79,6 +79,7 @@ class StudentDashboard extends Component {
             />
           ))}
           <NewSubmissionForm />
+          </Container>
           <Footer/>
       </React.Fragment>
     );
