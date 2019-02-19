45eec3e366a4be71e70aff85bd5ffec9b66f4aa8
Benjamin <hoppeconnect@gmail.com>
2019-02-19 10:19:48 -0500

    moved event list styles to admin.css

--- src/components/AdminComponents/AdminEventsList.jsx
+++ src/components/AdminComponents/AdminEventsList.jsx
@@ -1,37 +1,6 @@
 // CURRENTLY NOT BEING USED
-
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-const EventsCard = styled.div`
-  background-color: #64646C;
-  margin: 20px 10px;
-  width: 300px;
-  height: 400px;
-  border-radius: 2px;
-  grid-column-start: 3;
-  gird-row-start: 1;
-`
-
-// MAY NEED THIS
-// -------------------------------------------
-// const Container = styled.div`
-//   background-color: inherit
-//   margin: 20px 10px;
-//   width: 25vw;
-//   border-radius: 2px;
-//   grid-column-start: 1;
-//   justify-self: center;
-//   transition: 0.3s;
-//   opacity: 1;
-//   cursor: pointer;
-//   display: flex;
-//   justify-content: flex-start;
-//
-//   :hover {
-//     opacity: 0.5;
-//     transition: 0.3s;
-// }`
+import AdminStyles from './../../Admin.css'
 
 class AdminEventsList extends Component {
   constructor(props) {
@@ -43,9 +12,9 @@ class AdminEventsList extends Component {
   render() {
 
   return (
-    <EventsCard>
+    <div className="eventsList">
       <p>Events</p>
-    </EventsCard>
+    </div>
     );
   }
 }
