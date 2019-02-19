16ecdc751e12374c5a042be70beb8534dcef54e5
Benjamin <hoppeconnect@gmail.com>
2019-02-19 08:04:54 -0500

    moved footer styles into common.css

--- src/components/CommonComponents/Footer.jsx
+++ src/components/CommonComponents/Footer.jsx
@@ -1,41 +1,16 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import styled from 'styled-components';
-
-
-const Footers = styled.footer`
-  left: 0 ; right: 0; bottom: 0;
-  width: 100%;
-  height: 80px;
-  background-color: #2b2e34;
-  margin-top: 80px;
-  text-align: center;
-`
-const Text = styled.p`
-  display: inline-block;
-  color: white;
-  padding: 0px 10px;
-  background-color: inherit;
-  margin-top: 30px;
-  transition: color 0.5s;
-
-  :hover {
-    transition: color 0.5s;
-    color: #FC6404;
-}`
-
+import CommonStyles from './../../Common.css'
 
 const Footer = (props) => {
 
   return (
-    <React.Fragment>
-    <Footers>
-      <Link to="./admin"><Text>Admin Dashboard</Text></Link>
-      <Link to="./student"><Text>Student Dashboard</Text></Link>
-      <Link to="./signup"><Text>Sign up</Text></Link>
-      <Link to="./login"><Text>Log in</Text></Link>
-    </Footers>
-    </React.Fragment>
+    <footer>
+      <Link className="footerText" to="./admin">Admin Dashboard</Link>
+      <Link className="footerText" to="./student">Student Dashboard</Link>
+      <Link className="footerText" to="./signup">Sign up</Link>
+      <Link className="footerText" to="./login">Log in</Link>
+    </footer>
    );
 }
 
