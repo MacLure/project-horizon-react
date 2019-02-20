aaa67b8dd490e01dcf70266b392cd767b378ad3a
Benjamin <hoppeconnect@gmail.com>
2019-02-20 11:07:34 -0500

    working on signup page

--- src/components/CommonComponents/Signup.jsx
+++ src/components/CommonComponents/Signup.jsx
@@ -14,29 +14,29 @@ class Signup extends Component {
           <div className="signupContainer">
           <form className="loginForm" method="post">
             <label htmlfor="first_name">First Name</label>
-            <input className="loginInput" type="text" name="first_name"></input>
+            <input className="loginInput" type="text" name="first_name" placeholder="Sally"></input>
 
             <label className="loginLabel" htmlfor="last_name">Last Name</label>
-            <input className="loginInput" type="text" name="last_name"></input>
+            <input className="loginInput" type="text" name="last_name" placeholder="McAdmin"></input>
 
             <label className="loginLabel" htmlfor="email">Email</label>
-            <input className="loginInput" type="text" name="email"></input>
+            <input className="loginInput" type="text" name="email" placeholder="Sally.McAdmin@horizon.com"></input>
 
             <label className="loginLabel" htmlfor="phone">Phone Number</label>
-            <input className="loginInput" type="text" name="phone"></input>
+            <input className="loginInput" type="text" name="phone" placeholder="555-555-5555"></input>
 
             <label className="loginLabel" htmlfor="title">Title</label>
-            <input className="loginInput" type="text" name="title"></input>
+            <input className="loginInput" type="text" name="title" placeholder="Instructor"></input>
 
             <label className="loginLabel" htmlfor="password">Password</label>
-            <input className="loginInput" type="password" name="password"></input>
+            <input className="loginInput" type="password" name="password" placeholder="password"></input>
 
             <label className="loginLabel" htmlfor="confirm_password">Confrim Password</label>
-            <input className="loginInput" type="password" name="confirm_password"></input>
+            <input className="loginInput" type="password" name="confirm_password" placeholder="password"></input>
 
             <br/><button className="loginSubmitButton" type="submit">Submit</button><br/>
 
-            <p>Already have an account?<br/> Log back in <Link to="/login">here</Link></p>
+            <p>Already have an account?<br/> Log back in <Link className="redirect" to="/login">here</Link></p>
           </form>
         </div>
       </div>
