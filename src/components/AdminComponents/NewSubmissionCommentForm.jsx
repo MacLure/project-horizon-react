a75bd9a5f2cf071abebc4837ed7f9d10cccf4313
Benjamin <hoppeconnect@gmail.com>
2019-02-20 08:55:41 -0500

    moved new submission comment form styles to admin.css

--- src/components/AdminComponents/NewSubmissionCommentForm.jsx
+++ src/components/AdminComponents/NewSubmissionCommentForm.jsx
@@ -1,106 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { connect } from 'react-redux';
 import  {createNewSubmissionComment} from '../service';
-
-
-const Container = styled.div`
-  background-color: #2A2C33;
-  margin-top: 10px;
-  width: 40vw;
-  height: 300px;
-  border-radius: 2px;
-`
-
-const Title = styled.h2 `
-  padding: 20px 0px 15px 0px;
-  background-color: inherit;
-  text-align: center;
-`
-
-const Form = styled.form`
-  text-align: left;
-  background-color: transparent;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 3;
-  padding-left: 100px;
-`
-
-const Name = styled.div`
-  grid-column-start: 1;
-  grid-row-start: 1;
-  background-color: inherit;
-`
-
-const Course = styled.div`
-  grid-column-start: 2;
-  grid-row-start: 1;
-  background-color: inherit;
-`
-const StartDate = styled.div`
-  grid-column-start: 1;
-  grid-row-start: 2;
-  background-color: inherit;
-`
-
-const EndDate = styled.div`
-  grid-column-start: 2;
-  grid-row-start: 2;
-  background-color: inherit;
-`
-
-const Label = styled.label`
-  display: block;
-  padding: 15px 0;
-  background-color: inherit;
-  color: white;
-`
-
-const Input = styled.input`
-  border: 1px solid black;
-  border-radius: 2px;
-  padding: 5px 5px;
-  transition: border 0.5s;
-
-  :focus {
-    border: 1px solid #F6744E;
-    transition: border 0.5s;
-  }
-`
-
-const Select = styled.select`
-  border: 1px solid black;
-  border-radius: 2px;
-  padding: 5px 5px;
-  transition: border 0.5s;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;       /* remove default arrow */
-  background-image: src={Arrow};   /* add custom arrow */
-
-  :focus {
-    border: 1px solid #F6744E;
-    transition: border 0.5s;
-  }
-`
-const Button = styled.button`
-  margin: 20px auto;
-  cursor: pointer;
-  padding: 5px 10px;
-  background-color: #17B57E;
-  border: none;
-  transition: 0.3s;
-  border-radius: 2px;
-  font-size: 1.1em;
-  text-align: center;
-  grid-column-start: 1;
-  grid-row-start: 3;
-
-  :hover {
-    box-shadow: 0 0 15px black;
-    transition: box-shadow 0.3s;
-  }`
+import AdminStyles from './../../Admin.css';
 
 class NewSubmissionCommentForm extends Component {
   constructor(props) {
@@ -131,18 +32,23 @@ class NewSubmissionCommentForm extends Component {
 
   render() {
     return (
-      <React.Fragment>
-      <Container>
-      <Title>New Submission Cmment</Title>
-      <Form onSubmit={this.handleSubmit}>
-        <div>
-          <Label htmlFor="body">Body</Label>
-          <Input type="text_area" name="body" value={'body here'} onChange={this.handleChange} ></Input>
+      <div className="modal">
+      <div className="modalContainer">
+        <h2 className="formTitle">New Submission Cmment</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className="one">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title">
           </div>
-          <Button type="submit">Submit</Button>
-      </Form>
-      </Container>
-    </React.Fragment>
+          <div className="textarea">
+            <label htmlFor="body">Comment</label>
+            <textarea rows="7" cols="30" name="body" placeholder="body" value={this.state.body} onChange={this.handleChange}></textarea>
+            <br/>
+            <button className="submitButton" type="submit">Submit</button>
+          </div>
+      </form>
+    </div>
+    </div>
    );
   }
 }
