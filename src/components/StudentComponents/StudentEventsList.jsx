cc8f826c7ab761e7dbd7aa9f303277e9603f47fd
Benjamin <hoppeconnect@gmail.com>
2019-02-25 12:43:21 -0500

    working on student dsashboard

--- src/components/StudentComponents/StudentEventsList.jsx
+++ src/components/StudentComponents/StudentEventsList.jsx
@@ -2,26 +2,6 @@ import React, { Component } from 'react';
 import StudentStyles from './../../Student.css'
 import StudentEvent from './StudentEvent';
 
-// MAY NEED THIS
-// -------------------------------------------
-// const Container = styled.div`
-//   background-color: inherit
-//   margin: 20px 10px;
-//   width: 25vw;
-//   border-radius: 2px;
-//   grid-column-start: 1;
-//   justify-self: center;
-//   transition: 0.3s;
-//   opacity: 1;
-//   cursor: pointer;
-//   display: flex;
-//   justify-content: flex-start;
-//
-//   :hover {
-//     opacity: 0.5;
-//     transition: 0.3s;
-// }`
-
 class StudentEventsList extends Component {
   constructor(props) {
     super(props)
@@ -33,7 +13,7 @@ class StudentEventsList extends Component {
   options = {year: 'numeric', month: 'short', day: 'numeric' };
 
 
-  
+
   render() {
     const events = this.props.events
 
@@ -42,11 +22,11 @@ class StudentEventsList extends Component {
     {events.map( event => (
       <div
         className="eventItem"
-        key={event.id} 
+        key={event.id}
         onClick={()=>{this.props.TriggerEventDetails(event)}}>
         <p>{event.name}</p>
         <p>{new Date(Date.parse(event.date)).toLocaleString('en', this.options)} @ {new Date(Date.parse(event.time)).getHours()}:{new Date(Date.parse(event.time)).getMinutes()}</p>
-        
+
       </div>
         ))}
     </div>
