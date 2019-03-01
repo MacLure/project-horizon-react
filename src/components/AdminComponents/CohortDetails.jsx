fce9e67009708f983fa2a2f2e69cbf22851d234f
Benjamin <hoppeconnect@gmail.com>
2019-03-01 11:14:26 -0500

    made new add button for cohort details sections

--- src/components/AdminComponents/CohortDetails.jsx
+++ src/components/AdminComponents/CohortDetails.jsx
@@ -182,7 +182,7 @@ class CohortDetails extends Component {
       <div className="header">
         <h2 className="detailsTitle">{getCohortType(this.props.onFocusData!=null?this.props.onFocusData.course_type:null)} | {this.props.onFocusData!=null?this.props.onFocusData.name:null}</h2>
         <div className="dates">{this.props.onFocusData!=null?this.formattedStartDate:null} - {this.props.onFocusData!=null?this.formattedEndDate:null}</div>
-        <div className="editCohortButton" onClick={e=>{this.props.TriggerEditCohortForm(e)}} ><img class="editIcon" src={edit}/></div>
+        <div className="editCohortButton" onClick={e=>{this.props.TriggerEditCohortForm(e)}} ><img className="editIcon" src={edit}/></div>
         <div className="adminDeleteButton" onClick={e=>{this.handleDelete(e)}}><img className="deleteIcon" src={trash}/></div>
       </div>
       <div className="hr"></div>
@@ -190,22 +190,21 @@ class CohortDetails extends Component {
         <div className="students">
           <h2 className="sectionTitle">Students</h2>
             <div className="addButton" onClick={e=>{this.props.TriggerNewStudentForm(e)}}><img className="addIcon" src={plus}/></div>
-          <div >
+          <div>
             {this.cStudents}
           </div>
-          <div className="addButton" onClick={e=>{this.props.TriggerNewStudentForm(e)}}><img className="addIcon" src={plus}/></div>
         </div>
         <div className="assignments">
           <h2 className="sectionTitle">Assignments</h2>
+            <div className="addButton" onClick={e=>{this.props.TriggerNewAssignmentForm(e)}}><img className="addIcon" src={plus}/></div>
             <div>{this.cAssignments}</div>
-          <button className="addButton" onClick={e=>{this.props.TriggerNewAssignmentForm(e)}} >new assignment</button>
         </div>
         <div className="events">
           <h2 className="sectionTitle">Events</h2>
+            <div className="addButton" onClick={e=>{this.props.TriggerNewEventForm(e)}}><img className="addIcon" src={plus}/></div>
           <ul>
             {this.cEvents}
           </ul>
-          <div className="addButton" onClick={e=>{this.props.TriggerNewEventForm(e)}}>new event</div>
         </div>
       </div>
     </div>
