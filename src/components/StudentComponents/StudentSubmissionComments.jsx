65bcd825d2ed21d0e65cdde3ac8e36dff5a23d56
Malcolm <malcolm.maclure@gmail.com>
2019-03-09 17:52:14 -0500

    submittion comment styling

--- src/components/StudentComponents/StudentSubmissionComments.jsx
+++ src/components/StudentComponents/StudentSubmissionComments.jsx
@@ -24,14 +24,19 @@ const comments = this.props.comments.filter(comment => comment.submission_id ===
 const options = {year: 'numeric', month: 'short', day: 'numeric' };
 
   return (
-    <div>
+    <div >
     <h2 className="sectionTitle">Outcomes Comments</h2>
-    {comments.map(comment =>
-        <div key={comment.id}>
-          {admins.filter(admin => admin.id === comment.admin_id)[0].first_name} {admins.filter(admin => admin.id === comment.admin_id)[0].last_name} <div className="inlineDate">({new Date(Date.parse(comment.created_at)).toLocaleString('en', options)})</div>
-          <div>{comment.body}</div>
-        </div>)
-      }
+        {comments.map(comment =>
+        <div key={comment.id} className="submittionComment">
+          <div>
+            <div>{admins.filter(admin => admin.id === comment.admin_id)[0].first_name} {admins.filter(admin => admin.id === comment.admin_id)[0].last_name}</div>
+            <div className="inlineDate">{new Date(Date.parse(comment.created_at)).toLocaleString('en', options)}</div>
+          </div>
+          <div className="submissionCommentBody">
+            {comment.body}
+          </div>
+        </div>
+      )}
     </div>
     );
   }
