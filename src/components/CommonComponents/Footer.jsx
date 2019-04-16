aee5a65426b44035b84f2ed3ec8373292174d1eb
Benjamin <hoppeconnect@gmail.com>
2019-04-16 13:14:56 -0400

    moved copyright to right list

--- src/components/CommonComponents/Footer.jsx
+++ src/components/CommonComponents/Footer.jsx
@@ -12,13 +12,10 @@ const Footer = (props) => {
         <li><a target="_blank" href="https://benjaminhoppe.co/">Benjamin Hoppe</a></li>
       </ul>
 
-      <div className="copyright">
-        <p>&copy; Horizon 2019</p>
-      </div>
-
       <ul className="footerStuff">
         <li><a href="#">Privacy Policy</a></li>
         <li><a href="#">Legal Stuff</a></li>
+        <li>&copy; Horizon 2019</li>
       </ul>
     </footer>
    );
