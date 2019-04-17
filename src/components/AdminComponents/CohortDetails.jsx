ce9b9f47e82c6cc71988eb3ce485e6675610d82c
Malcolm <malcolm.maclure@gmail.com>
2019-04-17 13:16:14 -0400

    changed student progress colors

--- src/components/AdminComponents/CohortDetails.jsx
+++ src/components/AdminComponents/CohortDetails.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import NewStudentForm from './NewStudentForm'
-import NewAdminForm from './NewAdminForm'
-import NewEventForm from './NewEventForm'
-import NewAssignmentForm from './NewAssignmentForm'
-import edit from '../../assets/Icons/edit.svg';
-import trash from '../../assets/Icons/trash.svg';
-import plus from '../../assets/Icons/plusSmall.svg';
-import { connect } from 'react-redux';
-import { deleteCohort } from '.././../service';
-import AdminStyles from './../../Admin.css';
+import React, { Component } from "react";
+import NewStudentForm from "./NewStudentForm";
+import NewAdminForm from "./NewAdminForm";
+import NewEventForm from "./NewEventForm";
+import NewAssignmentForm from "./NewAssignmentForm";
+import edit from "../../assets/Icons/edit.svg";
+import trash from "../../assets/Icons/trash.svg";
+import plus from "../../assets/Icons/plusSmall.svg";
+import { connect } from "react-redux";
+import { deleteCohort } from ".././../service";
+import AdminStyles from "./../../Admin.css";
 
 class CohortDetails extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state ={
+    this.state = {
       showNewStudentForm: false,
       showNewAdminForm: false,
       showNewEventForm: false,
@@ -24,8 +24,8 @@ class CohortDetails extends Component {
       assignments: props.cohortAssignments,
       submissions: props.submissions,
       events: props.events,
-      selectedEvent: null,
-    }
+      selectedEvent: null
+    };
 
     // this.handleShowNewStudentForm = this.handleShowNewStudentForm.bind(this);
     this.handleshowNewAdminForm = this.handleshowNewAdminForm.bind(this);
@@ -37,176 +37,267 @@ class CohortDetails extends Component {
     this.reload = this.reload.bind(this);
   }
 
-//  if(props.onFocusData != null){
-//   const {start_date, end_date, name, course_type} = props.onFocusData;
-//  }
+  //  if(props.onFocusData != null){
+  //   const {start_date, end_date, name, course_type} = props.onFocusData;
+  //  }
 
-  students = this.props.students
-  cStudents = 'Loading...';
-  cEvents = 'Loading...';
-  cAssignments = 'Loading...';
+  students = this.props.students;
+  cStudents = "Loading...";
+  cEvents = "Loading...";
+  cAssignments = "Loading...";
 
-  options = {year: 'numeric', month: 'short', day: 'numeric' };
-  formattedStartDate = new Date(Date.parse(this.props.onFocusData.start_date)).toLocaleString('en', this.options)
-  formattedEndDate = new Date(Date.parse(this.props.onFocusData.end_date)).toLocaleString('en', this.options)
+  options = { year: "numeric", month: "short", day: "numeric" };
+  formattedStartDate = new Date(
+    Date.parse(this.props.onFocusData.start_date)
+  ).toLocaleString("en", this.options);
+  formattedEndDate = new Date(
+    Date.parse(this.props.onFocusData.end_date)
+  ).toLocaleString("en", this.options);
 
-  handleShowNewStudentForm = () =>{
-    this.setState({showNewStudentForm: !this.state.showNewStudentForm})
-  }
+  handleShowNewStudentForm = () => {
+    this.setState({ showNewStudentForm: !this.state.showNewStudentForm });
+  };
 
-  handleshowNewAdminForm = () =>{
-    this.setState({showNewAdminForm: !this.state.showNewAdminForm})
-  }
+  handleshowNewAdminForm = () => {
+    this.setState({ showNewAdminForm: !this.state.showNewAdminForm });
+  };
 
-  handleshowNewEventForm = () =>{
-    this.setState({showNewEventForm: !this.state.showNewEventForm})
-  }
+  handleshowNewEventForm = () => {
+    this.setState({ showNewEventForm: !this.state.showNewEventForm });
+  };
 
   // handleshowNewAssignmentForm = () =>{
   //   this.setState({showNewAssignmentForm: !this.state.showNewAssignmentForm})
   // }
 
-  handleDelete = (e) => {
+  handleDelete = e => {
     e.preventDefault();
-    let cohortId = this.props.onFocusData.id
-    deleteCohort(cohortId, this.props.token)
-    .then(e=>this.props.deleteSuccess())
-  }
+    let cohortId = this.props.onFocusData.id;
+    deleteCohort(cohortId, this.props.token).then(e =>
+      this.props.deleteSuccess()
+    );
+  };
 
-  handleEventClick = (e) => {
+  handleEventClick = e => {
     this.setState({
       selectedEvent: e
-    })
-  }
+    });
+  };
 
-  reload = () =>{
-    this.setState(this.state)
-  }
+  reload = () => {
+    this.setState(this.state);
+  };
 
   render() {
-
-    const options = {year: 'numeric', month: 'short', day: 'numeric' };
-    const formattedAssignmentDate = (date) => new Date(Date.parse(date)).toLocaleString('en', this.options)
-    const formattedEventDate = (date) => new Date(Date.parse(date)).toLocaleString('en', this.options)
-    const EventHour = (event) => new Date(Date.parse(event.time)).getHours()
-    const EventMinute = (event) => new Date(Date.parse(event.time)).getMinutes()
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    const formattedAssignmentDate = date =>
+      new Date(Date.parse(date)).toLocaleString("en", this.options);
+    const formattedEventDate = date =>
+      new Date(Date.parse(date)).toLocaleString("en", this.options);
+    const EventHour = event => new Date(Date.parse(event.time)).getHours();
+    const EventMinute = event => new Date(Date.parse(event.time)).getMinutes();
 
     const showNewStudentForm = () => {
-      if (this.state.showNewStudentForm ) {
-        return (
-          <NewStudentForm cohortId = {this.props.onFocusData.id} />
-        )
+      if (this.state.showNewStudentForm) {
+        return <NewStudentForm cohortId={this.props.onFocusData.id} />;
       }
-    }
+    };
 
     const handleshowNewAdminForm = () => {
-      if (this.state.showNewAdminForm ) {
-        return (
-          <NewAdminForm />
-        )
+      if (this.state.showNewAdminForm) {
+        return <NewAdminForm />;
       }
-    }
+    };
 
     const handleshowNewEventForm = () => {
-      if (this.state.showNewEventForm ) {
-        return (
-          <NewEventForm cohortId = {this.props.onFocusData.id} />
-        )
+      if (this.state.showNewEventForm) {
+        return <NewEventForm cohortId={this.props.onFocusData.id} />;
       }
-    }
+    };
 
     const handleshowNewAssignmentForm = () => {
-      if (this.state.showNewAssignmentForm ) {
+      if (this.state.showNewAssignmentForm) {
         return (
           <NewAssignmentForm
-            cohortId = {this.props.onFocusData.id}
-            assignmentSuccess = {this.reload}
+            cohortId={this.props.onFocusData.id}
+            assignmentSuccess={this.reload}
           />
-        )
+        );
       }
-    }
+    };
 
-    const getStudentProgressPercent = (student) => {
-      return this.state.submissions.filter(submission => submission.student_id === student.id).length <= 0 ?
-      0 :
-      Math.round(this.state.submissions.filter(submission => submission.student_id === student.id).length / this.state.assignments.length * 100)
-    }
+    const getStudentProgressPercent = student => {
+      return this.state.submissions.filter(
+        submission => submission.student_id === student.id
+      ).length <= 0
+        ? 0
+        : Math.round(
+            (this.state.submissions.filter(
+              submission => submission.student_id === student.id
+            ).length /
+              this.state.assignments.length) *
+              100
+          );
+    };
 
-    const getStudentProgressColor = (percent) => {
-      if (percent < 60) return "#DA101B"
-      if (percent >= 60 && percent < 80) return "#FCE404"
-      if (percent >= 80) return "#17B57E"
-    }
+    const getStudentProgressColor = percent => {
+      if (percent < 60) return "#DA101B";
+      if (percent >= 60 && percent < 80) return "#FCE404";
+      if (percent >= 80) return "#17B57E";
+    };
 
-  if(this.props.cohortStudents){
-    this.cStudents = this.props.cohortStudents.map(student => (
-      <li className="detailsListItem" key={student.id} onClick={e=>{this.props.TriggerStudentDetails(student.id)}}>
-        <div className="adminStudentName">
-          {student.first_name} {student.last_name}
-          <div className="studentProgressPercent" style={{backgroundColor: getStudentProgressColor(getStudentProgressPercent(student))}}>
-            {getStudentProgressPercent(student)}%
+    const getStudentProgressTextColor = percent => {
+      if (percent < 60) return "white";
+      if (percent >= 60 && percent < 80) return "black";
+      if (percent >= 80) return "white";
+    };
+
+    if (this.props.cohortStudents) {
+      this.cStudents = this.props.cohortStudents.map(student => (
+        <li
+          className="detailsListItem"
+          key={student.id}
+          onClick={e => {
+            this.props.TriggerStudentDetails(student.id);
+          }}
+        >
+          <div className="adminStudentName">
+            {student.first_name} {student.last_name}
+            <div
+              className="studentProgressPercent"
+              style={{
+                backgroundColor: getStudentProgressColor(
+                  getStudentProgressPercent(student)
+                ),
+                color: getStudentProgressTextColor(
+                  getStudentProgressPercent(student)
+                )
+              }}
+            >
+              {getStudentProgressPercent(student)}%
+            </div>
           </div>
-        </div>
-      </li>
-    ))
-  }
+        </li>
+      ));
+    }
 
-  if(this.props.cohortEvents){
-    this.cEvents = this.props.cohortEvents.map(event => (
-      <li className="detailsListItem" key={event.id} onClick={e=>{this.props.TriggerEventDetails(event)}} >
-        <div>{event.event_type}: {event.name}</div>
-        <div className="eventAssignmentDates">{formattedEventDate(event.date)} @ {EventHour(event)}:{EventMinute(event)}</div>
-      </li>
-    ))
-  }
-
-  if(this.props.cohortAssignments){
-    this.cAssignments = this.props.cohortAssignments.map(assignment => (
-      <li className="detailsListItem" key={assignment.id} onClick={e=>{this.props.TriggerAssignmentDetails(assignment)}} >
-        <div>{assignment.name}</div>
-        <div className="eventAssignmentDates">{formattedAssignmentDate(assignment.due_date)}</div>
-      </li>
-    ))
-  }
-
-  const getCohortType = (str) => {
-    if (str === "wdi") {return "Web Development"}
-    if (str === "uxdi") {return "UX Design"}
-    if (str === "dsi") {return "Data Science"}
-
-  }
-
-  return (
-    <div className="cohortDetailsSection">
-      <div className="header">
-        <h2 className="detailsTitle">{getCohortType(this.props.onFocusData!=null?this.props.onFocusData.course_type:null)} | {this.props.onFocusData!=null?this.props.onFocusData.name:null}</h2>
-        <div className="dates">{this.props.onFocusData!=null?this.formattedStartDate:null} - {this.props.onFocusData!=null?this.formattedEndDate:null}</div>
-        <div className="adminDeleteButton" onClick={e=>{this.handleDelete(e)}}><img className="deleteIcon" src={trash} alt="delete" /></div>
-      </div>
-      <div className="hr"></div>
-      <div className="cohortDetailsGrid">
-        <div className="students">
-          <h2 className="sectionTitle">Students</h2>
-            <div className="addButton" onClick={e=>{this.props.TriggerNewStudentForm(e)}}><img className="addIcon" src={plus} alt="add" /></div>
+    if (this.props.cohortEvents) {
+      this.cEvents = this.props.cohortEvents.map(event => (
+        <li
+          className="detailsListItem"
+          key={event.id}
+          onClick={e => {
+            this.props.TriggerEventDetails(event);
+          }}
+        >
           <div>
-            {this.cStudents}
+            {event.event_type}: {event.name}
+          </div>
+          <div className="eventAssignmentDates">
+            {formattedEventDate(event.date)} @ {EventHour(event)}:
+            {EventMinute(event)}
+          </div>
+        </li>
+      ));
+    }
+
+    if (this.props.cohortAssignments) {
+      this.cAssignments = this.props.cohortAssignments.map(assignment => (
+        <li
+          className="detailsListItem"
+          key={assignment.id}
+          onClick={e => {
+            this.props.TriggerAssignmentDetails(assignment);
+          }}
+        >
+          <div>{assignment.name}</div>
+          <div className="eventAssignmentDates">
+            {formattedAssignmentDate(assignment.due_date)}
+          </div>
+        </li>
+      ));
+    }
+
+    const getCohortType = str => {
+      if (str === "wdi") {
+        return "Web Development";
+      }
+      if (str === "uxdi") {
+        return "UX Design";
+      }
+      if (str === "dsi") {
+        return "Data Science";
+      }
+    };
+
+    return (
+      <div className="cohortDetailsSection">
+        <div className="header">
+          <h2 className="detailsTitle">
+            {getCohortType(
+              this.props.onFocusData != null
+                ? this.props.onFocusData.course_type
+                : null
+            )}{" "}
+            |{" "}
+            {this.props.onFocusData != null
+              ? this.props.onFocusData.name
+              : null}
+          </h2>
+          <div className="dates">
+            {this.props.onFocusData != null ? this.formattedStartDate : null} -{" "}
+            {this.props.onFocusData != null ? this.formattedEndDate : null}
+          </div>
+          <div
+            className="adminDeleteButton"
+            onClick={e => {
+              this.handleDelete(e);
+            }}
+          >
+            <img className="deleteIcon" src={trash} alt="delete" />
           </div>
         </div>
-        <div className="assignments">
-          <h2 className="sectionTitle">Assignments</h2>
-            <div className="addButton" onClick={e=>{this.props.TriggerNewAssignmentForm(e)}}><img className="addIcon" src={plus} alt="add" /></div>
+        <div className="hr" />
+        <div className="cohortDetailsGrid">
+          <div className="students">
+            <h2 className="sectionTitle">Students</h2>
+            <div
+              className="addButton"
+              onClick={e => {
+                this.props.TriggerNewStudentForm(e);
+              }}
+            >
+              <img className="addIcon" src={plus} alt="add" />
+            </div>
+            <div>{this.cStudents}</div>
+          </div>
+          <div className="assignments">
+            <h2 className="sectionTitle">Assignments</h2>
+            <div
+              className="addButton"
+              onClick={e => {
+                this.props.TriggerNewAssignmentForm(e);
+              }}
+            >
+              <img className="addIcon" src={plus} alt="add" />
+            </div>
             <div>{this.cAssignments}</div>
-        </div>
-        <div className="events">
-          <h2 className="sectionTitle">Events</h2>
-            <div className="addButton" onClick={e=>{this.props.TriggerNewEventForm(e)}}><img className="addIcon" src={plus} alt="add" /></div>
-          <ul>
-            {this.cEvents}
-          </ul>
+          </div>
+          <div className="events">
+            <h2 className="sectionTitle">Events</h2>
+            <div
+              className="addButton"
+              onClick={e => {
+                this.props.TriggerNewEventForm(e);
+              }}
+            >
+              <img className="addIcon" src={plus} alt="add" />
+            </div>
+            <ul>{this.cEvents}</ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
   }
 }
 
@@ -217,14 +308,15 @@ const mapStatetoProps = state => {
     isAuthenticating: state.isAuthenticating,
     currentUser: state.currentUser,
     errors: state.errors
-  }
-}
+  };
+};
 
 const mapDispatchtoProps = dispatch => {
-  return  {
-    onTokenReceive: token => dispatch({ type: "SET_USER_TOKEN", payload: token})
-  }
-}
+  return {
+    onTokenReceive: token =>
+      dispatch({ type: "SET_USER_TOKEN", payload: token })
+  };
+};
 
 export default connect(
   mapStatetoProps,
