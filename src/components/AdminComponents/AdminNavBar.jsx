ba5b884cd26ba111997529a9468c52dbee84e99b
Malcolm <malcolm.maclure@gmail.com>
2019-04-17 14:50:23 -0400

    user details in admin navbar

--- src/components/AdminComponents/AdminNavBar.jsx
+++ src/components/AdminComponents/AdminNavBar.jsx
@@ -4,16 +4,21 @@ import Logo from "../../assets/img/text_orange.svg";
 import AdminStyles from "./../../Admin.css";
 
 class AdminNavBar extends Component {
-  state = {
-    admin: this.props.admin
-  };
   render() {
     return (
       <React.Fragment>
         <nav>
           <img className="icon" src={Logo} alt="Horizon" />
           <ul className="navList">
-            <li>{this.state.admin ? this.state.admin.first_ame : null}</li>
+            <li>
+              <div className="adminNavbarDetails">
+                {this.props.admin
+                  ? `${this.props.admin.first_name} ${
+                      this.props.admin.last_name
+                    }, ${this.props.admin.title}`
+                  : null}
+              </div>
+            </li>
             <NavLink to="/login" activeClassName="active">
               <li className="navItem">Log out</li>
             </NavLink>
