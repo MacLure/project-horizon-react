4887efebcd38245fd2629a99afe1db0eb0b2d4ac
Malcolm <malcolm.maclure@gmail.com>
2019-04-17 14:35:47 -0400

    admin dashboard styling

--- src/components/AdminComponents/CohortCard.jsx
+++ src/components/AdminComponents/CohortCard.jsx
@@ -1,64 +1,73 @@
-import React from 'react';
-import CohortProgressCircle from './CohortProgressCircle';
-import AdminStyles from './../../Admin.css'
+import React from "react";
+import CohortProgressCircle from "./CohortProgressCircle";
+import AdminStyles from "./../../Admin.css";
 
-const CohortCard = (props) => {
+const CohortCard = props => {
+  const { start_date, end_date, name, course_type, id } = props.data;
+  const formattedStartDate = new Date(Date.parse(start_date));
+  const formattedEndDate = new Date(Date.parse(end_date));
+  const options = { year: "numeric", month: "short", day: "numeric" };
 
-  const {start_date, end_date, name, course_type, id} = props.data
-  const formattedStartDate = new Date(Date.parse(start_date))
-  const formattedEndDate = new Date(Date.parse(end_date))
-  const options = {year: 'numeric', month: 'short', day: 'numeric' };
-
-  const courseDays = Math.trunc((Date.parse(end_date) - Date.parse(start_date)) / (1000 * 60 * 60 * 24))
-  const daysLeft = Math.trunc((Date.parse(end_date) - Date.now()) / (1000 * 60 * 60 * 24))
+  const courseDays = Math.trunc(
+    (Date.parse(end_date) - Date.parse(start_date)) / (1000 * 60 * 60 * 24)
+  );
+  const daysLeft = Math.trunc(
+    (Date.parse(end_date) - Date.now()) / (1000 * 60 * 60 * 24)
+  );
 
   const daysLeftDisplay = () => {
-    if (daysLeft > courseDays){
-      return `Starts ${formattedStartDate.toLocaleString('en', options)}`
-    } else if (daysLeft <= 0 ) {
-      return `Ended ${formattedEndDate.toLocaleString('en', options)}`
-    } else if (daysLeft > 0){
-      return `${daysLeft} days left`
+    if (daysLeft > courseDays) {
+      return `Starts ${formattedStartDate.toLocaleString("en", options)}`;
+    } else if (daysLeft <= 0) {
+      return `Ended ${formattedEndDate.toLocaleString("en", options)}`;
+    } else if (daysLeft > 0) {
+      return `${daysLeft} days left`;
     }
-  }
+  };
 
   const courseProgress = () => {
     if (daysLeft <= 0) return 100;
     else if (daysLeft > courseDays) return 0;
     else if (daysLeft > 0) {
-      return Math.round( 100 - daysLeft / courseDays * 100)
+      return Math.round(100 - (daysLeft / courseDays) * 100);
     }
-  }
+  };
 
-  const getDescriptiveCourseType = (cohortName) => {
+  const getDescriptiveCourseType = cohortName => {
     if (cohortName === "wdi") {
-      return "Web Development"
+      return "Web Development";
     } else if (cohortName === "uxdi") {
-      return "UX Design"
+      return "UX Design";
     } else if (cohortName === "dsi") {
-      return "Data Science"
-  }
-}
+      return "Data Science";
+    }
+  };
 
   return (
-    <React.Fragment>
-      <div className="card"
-        style={{transform:props.isActive ? "translateY(-10px)": ""}}
-        onClick={e=>{props.onCohortClick(props.data)}}>
+    <div
+      className="card"
+      style={{
+        transform: props.isActive ? "translateY(-10px)" : "",
+        backgroundColor: props.isActive ? "#e6e6e6" : ""
+      }}
+      onClick={e => {
+        props.onCohortClick(props.data);
+      }}
+    >
       <div className="cardGrid">
         <div className="cardGridLeft">
-          <h3 className="courseType">{getDescriptiveCourseType(course_type)}</h3>
+          <h3 className="courseType">
+            {getDescriptiveCourseType(course_type)}
+          </h3>
           <h3 className="cohortName">{name}</h3>
           <p className="daysLeft">{daysLeftDisplay()}</p>
         </div>
         <div className="progress">
-          <CohortProgressCircle progress={courseProgress}/>
+          <CohortProgressCircle progress={courseProgress} />
         </div>
       </div>
-      </div>
-
-    </React.Fragment>
-   );
-  }
+    </div>
+  );
+};
 
 export default CohortCard;
