10f73ca9660a6999f3f26ea5ff3c30128d52d1aa
Malcolm <malcolm.maclure@gmail.com>
2019-04-17 14:15:46 -0400

    student submission form

--- src/components/StudentComponents/NewSubmissionForm.jsx
+++ src/components/StudentComponents/NewSubmissionForm.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import {connect} from 'react-redux';
-import  {createNewSubmission} from './../../service';
-
+import React, { Component } from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { createNewSubmission } from "./../../service";
 
 const Container = styled.div`
   margin-top: 10px;
   width: 40vw;
   height: 300px;
   border-radius: 2px;
-`
+`;
 
-const Title = styled.h2 `
+const Title = styled.h2`
   padding: 20px 0px 15px 0px;
   background-color: inherit;
   text-align: center;
-`
+`;
 
 const Form = styled.form`
   text-align: left;
@@ -24,49 +23,50 @@ const Form = styled.form`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 3;
   padding-left: 100px;
-`
+`;
 
 const Name = styled.div`
   grid-column-start: 1;
   grid-row-start: 1;
   background-color: inherit;
-`
+`;
 
 const Course = styled.div`
   grid-column-start: 2;
   grid-row-start: 1;
   background-color: inherit;
-`
+`;
 const StartDate = styled.div`
   grid-column-start: 1;
   grid-row-start: 2;
   background-color: inherit;
-`
+`;
 
 const EndDate = styled.div`
   grid-column-start: 2;
   grid-row-start: 2;
   background-color: inherit;
-`
+`;
 
 const Label = styled.label`
   display: block;
   padding: 15px 0;
   background-color: inherit;
   color: white;
-`
+`;
 
 const Input = styled.input`
   border: 1px solid black;
   border-radius: 2px;
   padding: 5px 5px;
   transition: border 0.5s;
+  background-color: #e6e6e6;
 
   :focus {
-    border: 1px solid #F6744E;
+    border: 1px solid #f6744e;
     transition: border 0.5s;
   }
-`
+`;
 
 const Select = styled.select`
   border: 1px solid black;
@@ -82,12 +82,20 @@ const Select = styled.select`
     border: 1px solid #F6744E;
     transition: border 0.5s;
   }
-`
+`;
+
+const TextArea = styled.textarea`
+  background-color: #e6e6e6;
+
+  :placeholder {
+    color: black;
+  }
+`;
 const Button = styled.button`
-  margin: 20px auto;
+  margin: 20px 0;
   cursor: pointer;
   padding: 5px 10px;
-  background-color: #17B57E;
+  background-color: #17b57e;
   border: none;
   transition: 0.3s;
   border-radius: 2px;
@@ -97,59 +105,72 @@ const Button = styled.button`
   grid-row-start: 3;
 
   :hover {
-    box-shadow: 0 0 15px black;
+    box-shadow: 0 0 5px grey;
     transition: box-shadow 0.3s;
-  }`
+  }
+`;
 
 class NewSubmissionForm extends Component {
   constructor(props) {
-  super(props);
-  this.state = {
-    url: '',
-    body: '',
-    assignment_id: this.props.assignment.id,
-    student_id: this.props.student.id,
-    is_approved: false
-   }
+    super(props);
+    this.state = {
+      url: "",
+      body: "",
+      assignment_id: this.props.assignment.id,
+      student_id: this.props.student.id,
+      is_approved: false
+    };
 
-   this.handleChange = this.handleChange.bind(this);
-   this.handleSubmit = this.handleSubmit.bind(this);
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
-  event.preventDefault();
-    let data = this.state
+    event.preventDefault();
+    let data = this.state;
     createNewSubmission(data, this.props.token)
-    .then(e=>e.json())
-    .then(e=>this.props.submissionSuccess())
+      .then(e => e.json())
+      .then(e => this.props.submissionSuccess());
   }
-
 
   render() {
     return (
-      <Container>
-      <h2 className="sectionTitle">Your Submission</h2>
-      <Form onSubmit={this.handleSubmit}>
-        <div>
-          <Label htmlFor="url">URL</Label>
-          <Input type="text" placeholder="www.mysubmission.com..." name="url" value={this.state.url} onChange={this.handleChange} ></Input>
-        </div>
-        <div>
-          <Label htmlFor="body">Body</Label>
-          <Input type="text_area" name="body" placeholder={'Your body text here (if applicable)'} value={this.state.body} onChange={this.handleChange} ></Input>
+      <div className="studentSubmissionContainer">
+        <h2 className="sectionTitle">Your Submission</h2>
+        <Form onSubmit={this.handleSubmit}>
+          <div>
+            <Label htmlFor="url">URL</Label>
+            <input
+              className="studentSubmissionURLInput"
+              type="text"
+              placeholder="www.mysubmission.com..."
+              name="url"
+              value={this.state.url}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <Label htmlFor="body">Body</Label>
+            <TextArea
+              type="text_area"
+              rows="10"
+              cols="40"
+              name="body"
+              placeholder={"Your body text here (if applicable)"}
+              value={this.state.body}
+              onChange={this.handleChange}
+            />
           </div>
           <Button type="submit">Submit</Button>
-      </Form>
-      </Container>
-   );
+        </Form>
+      </div>
+    );
   }
 }
-
 
 const mapStatetoProps = state => {
   return {
@@ -158,14 +179,15 @@ const mapStatetoProps = state => {
     isAuthenticating: state.isAuthenticating,
     currentUser: state.currentUser,
     errors: state.errors
-  }
-}
+  };
+};
 
 const mapDispatchtoProps = dispatch => {
-  return  {
-    onTokenReceive: token => dispatch({ type: "SET_USER_TOKEN", payload: token})
-  }
-}
+  return {
+    onTokenReceive: token =>
+      dispatch({ type: "SET_USER_TOKEN", payload: token })
+  };
+};
 
 export default connect(
   mapStatetoProps,
