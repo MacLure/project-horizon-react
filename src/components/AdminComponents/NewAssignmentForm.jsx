b82446a622aab18d096300a5456e0e999a4bc3ea
Malcolm <malcolm.maclure@gmail.com>
2019-04-20 16:37:06 -0400

    restyled login page

--- src/components/AdminComponents/NewAssignmentForm.jsx
+++ src/components/AdminComponents/NewAssignmentForm.jsx
@@ -1,61 +1,83 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import  {createNewAssignment} from './../../service';
-import AdminStyles from './../../Admin.css';
-import X from '../../assets/Icons/x.svg';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createNewAssignment } from "./../../service";
+import AdminStyles from "./../../Admin.css";
+import X from "../../assets/Icons/x.svg";
 
 class NewAssignmentForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cohort_id: this.props.cohortId,
-      name: '',
-      due_date: '',
-      body: ''
-   }
+      name: "",
+      due_date: "",
+      body: ""
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  handleSubmit = (e) =>{
+  handleSubmit = e => {
     e.preventDefault();
-    let data = this.state
+    let data = this.state;
     createNewAssignment(data, this.props.token)
-    .then(e=>e.json())
-    .then(e=>this.props.assignmentSuccess())
-    .then(this.props.escapeNewAssignmentModal)
-
-  }
+      .then(e => e.json())
+      .then(e => this.props.assignmentSuccess())
+      .then(this.props.escapeNewAssignmentModal);
+  };
 
   render() {
     return (
       <div className="modal">
-      <div className="modalContainer">
-       <div className="modalEscape"  onClick={this.props.escapeNewAssignmentModal}><img className="escapeIcon" src={X} alt="exit" /></div>
-           <h2 className="formTitle">Create Assignment</h2>
-          <form onSubmit={this.handleSubmit}>
-            <div className="one">
+        <div className="modalContainer">
+          <div
+            className="modalEscape"
+            onClick={this.props.escapeNewAssignmentModal}
+          >
+            <img className="escapeIcon" src={X} alt="exit" />
+          </div>
+          <h2 className="formTitle">Create Assignment</h2>
+          <form className="newAssignmentForm" onSubmit={this.handleSubmit}>
+            <div>
               <label htmlFor="name">Assignment Name</label>
-              <input type="text" name="name" placeholder="Personal Branding"  value={this.state.name} onChange={this.handleChange}></input>
-            </div>
-            <div className="two">
+              <input
+                type="text"
+                name="name"
+                placeholder="Personal Branding"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
               <label htmlFor="due_date">Due Date</label>
-              <input type="date" name="due_date" value={this.state.due_date} onChange={this.handleChange} ></input>
+              <input
+                type="date"
+                name="due_date"
+                value={this.state.due_date}
+                onChange={this.handleChange}
+              />
             </div>
-            <div className="textarea">
+            <div>
               <label htmlFor="body">Assignment Body</label>
-              <textarea rows="10" cols="32" name="body" placeholder="body" value={this.state.body} onChange={this.handleChange}></textarea>
-            <br/>
-            <button className="submitButton" type="submit">Submit</button>
+              <textarea
+                rows="10"
+                cols="32"
+                name="body"
+                placeholder="body"
+                value={this.state.body}
+                onChange={this.handleChange}
+              />
+              <br />
+              <button className="submitButton" type="submit">
+                Submit
+              </button>
             </div>
           </form>
-          </div>
         </div>
-     );
+      </div>
+    );
   }
 }
 
@@ -66,14 +88,15 @@ const mapStatetoProps = state => {
     isAuthenticating: state.isAuthenticating,
     currentUser: state.currentUser,
     errors: state.errors
-  }
-}
+  };
+};
 
 const mapDispatchtoProps = dispatch => {
-  return  {
-    onTokenReceive: token => dispatch({ type: "SET_USER_TOKEN", payload: token})
-  }
-}
+  return {
+    onTokenReceive: token =>
+      dispatch({ type: "SET_USER_TOKEN", payload: token })
+  };
+};
 
 export default connect(
   mapStatetoProps,
--- src/components/CommonComponents/Login.jsx
+++ src/components/CommonComponents/Login.jsx
@@ -1,148 +1,212 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import  {onAdminLogin} from '.././../service';
-import  {onStudentLogin} from '.././../service';
-import CommonStyles from './../../Common.css'
-import Logo from '../../assets/img/horizon_text2.svg';
-import loading from '../../assets/Icons/loading.svg';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { onAdminLogin } from ".././../service";
+import { onStudentLogin } from ".././../service";
+import CommonStyles from "./../../Common.css";
+import Logo from "../../assets/img/horizon_text2.svg";
+import loading from "../../assets/Icons/loading.svg";
 
 class Login extends Component {
   constructor(props) {
-    super(props)
-      this.state = {
-        user: null,
-        email: '',
-        password: '',
-        loader: false,
-        error: null,
-       }
+    super(props);
+    this.state = {
+      user: null,
+      email: "",
+      password: "",
+      loader: false,
+      error: null
+    };
   }
 
-  componentDidMount(){
-    let body = document.getElementsByTagName('body')[0];
+  componentDidMount() {
+    let body = document.getElementsByTagName("body")[0];
     body.style.paddingBottom = 0;
-    console.log(body)
   }
 
   onStudent = () => {
-    this.setState({user:'student'})
-  }
+    this.setState({ user: "student" });
+  };
 
   onAdmin = () => {
-    this.setState({user:'admin'})
-  }
+    this.setState({ user: "admin" });
+  };
 
   onSubmit = () => {
-    if (this.state.user === 'admin') {
+    if (this.state.user === "admin") {
       if (!this.state.email || !this.state.password) {
-        this.setState({error: "email or password missing"})
+        this.setState({ error: "email or password missing" });
       }
-      if (this.state.email !== '' && this.state.password !== '') {
-        this.setState({loader: true})
-        this.setState({error: null})
+      if (this.state.email !== "" && this.state.password !== "") {
+        this.setState({ loader: true });
+        this.setState({ error: null });
         onAdminLogin(this.state.email, this.state.password)
-        .then(e => e.json())
-        .then(e => {console.log(e)
-          this.props.onTokenReceive(e.jwt)
-          this.props.history.push('/admin')
-        })
-        .catch(e => {console.log('ERR: ', e)
-          this.setState({loader: false})
-          this.setState({error: "email or password incorrect"})
-        })
+          .then(e => e.json())
+          .then(e => {
+            console.log(e);
+            this.props.onTokenReceive(e.jwt);
+            this.props.history.push("/admin");
+          })
+          .catch(e => {
+            console.log("ERR: ", e);
+            this.setState({ loader: false });
+            this.setState({ error: "email or password incorrect" });
+          });
       }
-    } else if (this.state.user === 'student') {
+    } else if (this.state.user === "student") {
       if (!this.state.email || !this.state.password) {
-        this.setState({error: "email or password missing"})
+        this.setState({ error: "email or password missing" });
       }
-      if (this.state.email !== '' && this.state.password !== '') {
-        this.setState({loader: true})
-        this.setState({error: null})
+      if (this.state.email !== "" && this.state.password !== "") {
+        this.setState({ loader: true });
+        this.setState({ error: null });
         onStudentLogin(this.state.email, this.state.password)
-        .then(e => e.json())
-        .then(e => {console.log(e)
-          this.props.onTokenReceive(e.jwt)
-          this.props.history.push('/student')
-        })
-        .catch(e => {console.log('ERR: ', e)
-          this.setState({loader: false})
-          this.setState({error: "email or password incorrect"})
-        })
+          .then(e => e.json())
+          .then(e => {
+            console.log(e);
+            this.props.onTokenReceive(e.jwt);
+            this.props.history.push("/student");
+          })
+          .catch(e => {
+            console.log("ERR: ", e);
+            this.setState({ loader: false });
+            this.setState({ error: "email or password incorrect" });
+          });
       }
     }
-  }
+  };
 
   onGuestSubmit = () => {
-    this.setState({loader: true})
-    this.setState({error: null})
-    if (this.state.user === 'student') {
-      onStudentLogin('guest.student@horizon.com', 'password')
-      .then(e => e.json())
-      .then(e => {console.log(e)
-        this.props.onTokenReceive(e.jwt)
-        this.props.history.push('/student')
-      })
-      .catch(e => {console.log('ERR: ', e)
-        this.setState({loader: false})
-        this.setState({error: "Something went wrong. Please try again."})
-      })
+    this.setState({ loader: true });
+    this.setState({ error: null });
+    if (this.state.user === "student") {
+      onStudentLogin("guest.student@horizon.com", "password")
+        .then(e => e.json())
+        .then(e => {
+          console.log(e);
+          this.props.onTokenReceive(e.jwt);
+          this.props.history.push("/student");
+        })
+        .catch(e => {
+          console.log("ERR: ", e);
+          this.setState({ loader: false });
+          this.setState({ error: "Something went wrong. Please try again." });
+        });
     }
-    if (this.state.user === 'admin') {
-      onAdminLogin('guest.admin@horizon.com', 'password')
-      .then(e => e.json())
-      .then(e => {console.log(e)
-        this.props.onTokenReceive(e.jwt)
-        this.props.history.push('/admin')
-      })
-      .catch(e => {console.log('ERR: ', e)
-        this.setState({loader: false})
-        this.setState({error: "Something went wrong. Please try again."})
-      })
+    if (this.state.user === "admin") {
+      onAdminLogin("guest.admin@horizon.com", "password")
+        .then(e => e.json())
+        .then(e => {
+          console.log(e);
+          this.props.onTokenReceive(e.jwt);
+          this.props.history.push("/admin");
+        })
+        .catch(e => {
+          console.log("ERR: ", e);
+          this.setState({ loader: false });
+          this.setState({ error: "Something went wrong. Please try again." });
+        });
     }
-  }
+  };
 
   displayForm = () => {
-    return (this.state.user === null) ?
-    null : <form className="loginForm" method="post">
-    <button className="loginSubmitButton" type="submit" onClick={e=>{e.preventDefault(); this.onGuestSubmit()}}>Log in as a guest {this.state.user}</button>
-    <p class="guestOrLogin">or:</p>
-        <label className="loginLabel" htmlFor="email">Email</label><input className="loginInput" placeholder="hello@horizon.com" type="text" name="email" onChange={e=>{
-          this.setState({email: e.target.value})
-        }}></input>
-        <label className="loginLabel" htmlFor="password">Password</label><input className="loginInput" placeholder="password" type="password" name="password" onChange={e=>{
-          this.setState({password: e.target.value})
-        }}></input>
-      <div className="notice">
-      {this.state.loader ? <img className="loader" src={loading}  alt="loading" /> : null}
-      {this.state.error ? this.state.error : null}
-      </div>
-      <button className="loginSubmitButton" type="submit" onClick={e=>{e.preventDefault(); this.onSubmit()}}>Log in </button>
+    return this.state.user === null ? null : (
+      <form className="loginForm" method="post">
+        <button
+          className="guestLoginSubmitButton"
+          type="submit"
+          onClick={e => {
+            e.preventDefault();
+            this.onGuestSubmit();
+          }}
+        >
+          Log in as a guest {this.state.user}
+        </button>
+        <hr />
+        <p class="guestOrLogin">or log in as a registered user:</p>
+        <label className="loginLabel" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="loginInput"
+          placeholder="hello@horizon.com"
+          type="text"
+          name="email"
+          onChange={e => {
+            this.setState({ email: e.target.value });
+          }}
+        />
+        <label className="loginLabel" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="loginInput"
+          placeholder="password"
+          type="password"
+          name="password"
+          onChange={e => {
+            this.setState({ password: e.target.value });
+          }}
+        />
+        <div className="notice">
+          {this.state.loader ? (
+            <img className="loader" src={loading} alt="loading" />
+          ) : null}
+          {this.state.error ? this.state.error : null}
+        </div>
+        <button
+          className="loginSubmitButton"
+          type="submit"
+          onClick={e => {
+            e.preventDefault();
+            this.onSubmit();
+          }}
+        >
+          Log in{" "}
+        </button>
       </form>
-  }
+    );
+  };
 
   highlightStudentButton = () => {
-    return (this.state.user === 'student') ? {backgroundColor: '#fff', color: '#FC6404'} : {}
-  }
+    return this.state.user === "student"
+      ? { backgroundColor: "#fff", color: "#FC6404" }
+      : {};
+  };
 
   highlightAdminButton = () => {
-    return (this.state.user === 'admin') ? {backgroundColor: '#fff', color: '#FC6404'} : {}
-  }
+    return this.state.user === "admin"
+      ? { backgroundColor: "#fff", color: "#FC6404" }
+      : {};
+  };
 
- render() {
-
-   return (
-    <div className="landing">
-      <div className="horizonContainer"><img className="horizon" src={Logo} alt="Horizon" /></div>
-      <div className="loginContainer">
-      <div className="buttonFlex">
-        <button className="selectButton" style={this.highlightStudentButton()} onClick={this.onStudent}>Student</button>
-        <button className="selectButton" style={this.highlightAdminButton()} onClick={this.onAdmin}>Admin</button>
+  render() {
+    return (
+      <div className="landing">
+        <div className="horizonContainer">
+          <img className="horizon" src={Logo} alt="Horizon" />
+        </div>
+        <div className="loginContainer">
+          <div className="buttonFlex">
+            <button
+              className="selectButton"
+              style={this.highlightStudentButton()}
+              onClick={this.onStudent}
+            >
+              Student
+            </button>
+            <button
+              className="selectButton"
+              style={this.highlightAdminButton()}
+              onClick={this.onAdmin}
+            >
+              Admin
+            </button>
+          </div>
+          {this.displayForm()}
+        </div>
       </div>
-      {this.displayForm()}
-      </div>
-    </div>
     );
- }
+  }
 }
 
 const mapStatetoProps = state => {
@@ -152,15 +216,17 @@ const mapStatetoProps = state => {
     isAuthenticating: state.isAuthenticating,
     currentUser: state.currentUser,
     errors: state.errors
-  }
-}
+  };
+};
 
 const mapDispatchtoProps = dispatch => {
-  return  {
-    onTokenReceive: token => dispatch({ type: "SET_USER_TOKEN", payload: token})
-  }
-}
+  return {
+    onTokenReceive: token =>
+      dispatch({ type: "SET_USER_TOKEN", payload: token })
+  };
+};
 
 export default connect(
   mapStatetoProps,
-  mapDispatchtoProps)(Login)
+  mapDispatchtoProps
+)(Login);
