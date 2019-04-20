5209eac3628d0c2a3ec4fbd5d124b3ad06de7772
Malcolm <malcolm.maclure@gmail.com>
2019-04-20 10:49:09 -0400

    created utilities file

--- src/components/AdminComponents/NewAdminForm.jsx
+++ src/components/AdminComponents/NewAdminForm.jsx
@@ -1,69 +1,99 @@
-import React, { Component } from 'react';
-import AdminStyles from './../../Admin.css';
-import X from '../../assets/Icons/x.svg';
+import React, { Component } from "react";
+import AdminStyles from "./../../Admin.css";
+import X from "../../assets/Icons/x.svg";
 
 class NewAdminForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      first_name: '',
-      last_name: '',
-      phone: '',
-      email: '',
-      cohort_id: '',
-      image_url: ''
-    }
+      first_name: "",
+      last_name: "",
+      phone: "",
+      email: "",
+      cohort_id: "",
+      image_url: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
 
   handleChange(event) {
-    this.setState({[event.target.name]: event.target.value.toLowerCase()});
+    this.setState({ [event.target.name]: event.target.value.toLowerCase() });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-      // fetch('http://localhost:3000/api/admin/cohorts', {
-      let data = this.state
-      console.log(data);
-        fetch(`https://project-horizon-rails.herokuapp.com/api/admin/students?student=${JSON.stringify(data)}`, {
-        method: 'post',
+    let data = this.state;
+    fetch(
+      `https://project-horizon-rails.herokuapp.com/api/admin/students?student=${JSON.stringify(
+        data
+      )}`,
+      {
+        method: "post",
         mode: "cors"
-      }).then(response => {console.log(this.state)})
-      .then(this.props.escapeNewAdminModal)
+      }
+    ).then(this.props.escapeNewAdminModal);
   }
 
- render() {
-   return (
-     <div className="modal">
-       <div className="modalContainer">
-       <div className="modalEscape"  onClick={this.props.escapeNewAdminModal}><img className="escapeIcon" src={X} alt="exit" /></div>
-        <h2 className="formTitle">Add Admin</h2>
-        <form method="post" onSubmit={this.handleSubmit}>
-          <div className="one">
-            <label htmlFor="first_name">First Name</label>
-            <input type="text" name="first_name" placeholder="John"  value={this.state.first_name} onChange={this.handleChange}></input>
+  render() {
+    return (
+      <div className="modal">
+        <div className="modalContainer">
+          <div className="modalEscape" onClick={this.props.escapeNewAdminModal}>
+            <img className="escapeIcon" src={X} alt="exit" />
           </div>
-          <div className="two">
-            <label htmlFor="last_name">Last Name</label>
-            <input type="text" name="last_name" placeholder="Smith"  value={this.state.last_name} onChange={this.handleChange}></input>
-          </div>
-          <div className="three">
-            <label htmlFor="phone">Phone Number</label>
-            <input type="tel" name="phone" placeholder="555-555-5555"  value={this.state.phone} onChange={this.handleChange}></input>
-          </div>
-          <div className="four">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" placeholder="hello@mail.com" value={this.state.email} onChange={this.handleChange}></input>
-          </div>
-         <br/><button className="submitButton" type="submit">Submit</button>
-       </form>
-      </div>
+          <h2 className="formTitle">Add Admin</h2>
+          <form method="post" onSubmit={this.handleSubmit}>
+            <div className="one">
+              <label htmlFor="first_name">First Name</label>
+              <input
+                type="text"
+                name="first_name"
+                placeholder="John"
+                value={this.state.first_name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="two">
+              <label htmlFor="last_name">Last Name</label>
+              <input
+                type="text"
+                name="last_name"
+                placeholder="Smith"
+                value={this.state.last_name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="three">
+              <label htmlFor="phone">Phone Number</label>
+              <input
+                type="tel"
+                name="phone"
+                placeholder="555-555-5555"
+                value={this.state.phone}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="four">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                placeholder="hello@mail.com"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <br />
+            <button className="submitButton" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     );
- }
+  }
 }
 
 export default NewAdminForm;
--- src/components/StudentComponents/StudentAssignmentsList.jsx
+++ src/components/StudentComponents/StudentAssignmentsList.jsx
@@ -1,74 +1,68 @@
-import React, { Component } from 'react';
-import StudentAssignment from './StudentAssignment';
-import StudentStyles from './../../Student.css'
-import SubmissionDetails from './SubmissionDetails';
-import NewSubmissionForm from './NewSubmissionForm';
-import StudentCohortDetails from './StudentCohortDetails';
-
+import React, { Component } from "react";
+import StudentAssignment from "./StudentAssignment";
+import StudentStyles from "./../../Student.css";
+import SubmissionDetails from "./SubmissionDetails";
+import NewSubmissionForm from "./NewSubmissionForm";
+import StudentCohortDetails from "./StudentCohortDetails";
 
 class StudentAssignmentsList extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       selectedAssignment: null,
       onFocusData: null,
       assignments: [],
       submissions: [],
-      submission_comments: [],
-    }
+      submission_comments: []
+    };
 
-    this.onAssignmentClick = this.onAssignmentClick.bind(this)
-
+    this.onAssignmentClick = this.onAssignmentClick.bind(this);
   }
 
-  reload = () =>{
-    console.log("OK")
-  }
+  reload = () => {};
 
-  onAssignmentClick = (data) => {
+  onAssignmentClick = data => {
     this.setState({
-      onFocusData:data,
-      selectedAssignment:this.props.assignments.indexOf(data)
-    })
+      onFocusData: data,
+      selectedAssignment: this.props.assignments.indexOf(data)
+    });
     this.props.getOnFocusData(data);
+  };
 
-  }
-
-  getAssignmentInfo = () => (
-    this.setState( {
+  getAssignmentInfo = () =>
+    this.setState({
       assignments: this.props.assignments,
       submissions: this.props.submissions,
-      submission_comments: this.props.submission_comments,
-    })
-  )
+      submission_comments: this.props.submission_comments
+    });
 
-
-
-// documentDidMount() {
-//   this.getAssignmentInfo()
-// }
-
-
+  // documentDidMount() {
+  //   this.getAssignmentInfo()
+  // }
 
   render() {
-
-  const {assignments, submissions, student, getOnFocusData, submissionComments} = this.props
+    const {
+      assignments,
+      submissions,
+      student,
+      getOnFocusData,
+      submissionComments
+    } = this.props;
 
     return (
       <div className="assignmentlist">
-      <StudentCohortDetails
-      studentFirstName={this.props.student.first_name}
-      studentLastName={this.props.student.last_name}
-      name={this.props.cohort.name}
-      course_type={this.props.cohort.course_type}
-      start_date={this.props.cohort.start_date}
-      end_date={this.props.cohort.end_date}
-      submissions={this.props.submissions}
-      assignments={this.props.assignments}
-
-      />
-      <h2 className="sectionTitle">Assignments</h2>
+        <StudentCohortDetails
+          studentFirstName={this.props.student.first_name}
+          studentLastName={this.props.student.last_name}
+          name={this.props.cohort.name}
+          course_type={this.props.cohort.course_type}
+          start_date={this.props.cohort.start_date}
+          end_date={this.props.cohort.end_date}
+          submissions={this.props.submissions}
+          assignments={this.props.assignments}
+        />
+        <h2 className="sectionTitle">Assignments</h2>
 
         {assignments.map((assignment, index) => (
           <StudentAssignment
@@ -80,14 +74,17 @@ class StudentAssignmentsList extends Component {
             data={assignment}
             onAssignmentClick={this.onAssignmentClick}
             isActive={this.state.selectedCohort === index}
-            submission={this.props.submissions.filter(submission => submission.assignment_id === assignment.id)[0]}
+            submission={
+              this.props.submissions.filter(
+                submission => submission.assignment_id === assignment.id
+              )[0]
+            }
             student={this.props.student}
           />
         ))}
-        <div style={{height:"10px", backgroundColor:"white"}} />
-
+        <div style={{ height: "10px", backgroundColor: "white" }} />
       </div>
-     );
+    );
   }
 }
 
