06cff7e042d8ef78cbd103f58a73022126a0a970
Malcolm <malcolm.maclure@gmail.com>
2019-04-20 16:06:05 -0400

    styling on new form

--- src/components/AdminComponents/NewCohortForm.jsx
+++ src/components/AdminComponents/NewCohortForm.jsx
@@ -1,70 +1,103 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import  {createNewCohort} from './../../service';
-import AdminStyles from './../../Admin.css';
-import X from '../../assets/Icons/x.svg';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createNewCohort } from "./../../service";
+import AdminStyles from "./../../Admin.css";
+import X from "../../assets/Icons/x.svg";
 
 class NewCohortForm extends Component {
   constructor(props) {
-  super(props);
-  this.state = {
-    name: '',
-    course_type: '',
-    start_date: '',
-    end_date: '',
-   }
+    super(props);
+    this.state = {
+      name: "",
+      course_type: "",
+      start_date: "",
+      end_date: ""
+    };
 
-   this.handleChange = this.handleChange.bind(this);
-   this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
-  event.preventDefault();
-    let data = this.state
+    event.preventDefault();
+    let data = this.state;
     createNewCohort(data, this.props.token)
-    .then(e=>e.json())
-    .then(e=>this.props.cohortSuccess())
-    .then(this.props.escapeNewCohortModal)
+      .then(e => e.json())
+      .then(e => this.props.cohortSuccess())
+      .then(this.props.escapeNewCohortModal);
   }
 
   render() {
-    document.body.style.backgroundColor = '#212229';
+    document.body.style.backgroundColor = "#212229";
     return (
       <div className="modal">
-      <div className="modalContainer">
-       <div className="modalEscape" onClick={this.props.escapeNewCohortModal}><img className="escapeIcon" src={X} alt="exit" /></div>
-       <h2 className="formTitle">New Cohort</h2>
-       <form onSubmit={this.handleSubmit}>
-        <div className="one">
-          <label htmlFor="name">Name</label>
-          <input type="text" placeholder="BB8" name="name" value={this.state.name} onChange={this.handleChange} ></input>
+        <div className="modalContainer">
+          <div
+            className="modalEscape"
+            onClick={this.props.escapeNewCohortModal}
+          >
+            <img className="escapeIcon" src={X} alt="exit" />
+          </div>
+          <h2 className="formTitle">New Cohort</h2>
+          <form onSubmit={this.handleSubmit}>
+            <div className="one">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                placeholder="cohort name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="two">
+              <label htmlFor="course_type">Course Type</label>
+              <select
+                name="course_type"
+                placeholder="Choose a course"
+                value={this.state.course_type}
+                onChange={this.handleChange}
+              >
+                <option value="" hidden>
+                  Select a course
+                </option>
+                <option value="wdi">Web Development </option>
+                <option value="uxdi">User Expierence</option>
+                <option value="dsi">Data Science</option>
+              </select>
+            </div>
+            <div className="three">
+              <label htmlFor="start_date">Start Date</label>
+              <input
+                type="date"
+                name="start_date"
+                value={this.state.start_date}
+                onChange={this.handleChange}
+              />
+              <br />
+              <button className="submitButton" type="submit">
+                Submit
+              </button>
+            </div>
+            <div className="four">
+              <label htmlFor="end_date" placeholder="yyyy-mm-dd">
+                End Date
+              </label>
+              <input
+                type="date"
+                name="end_date"
+                value={this.state.end_date}
+                onChange={this.handleChange}
+              />
+            </div>
+          </form>
         </div>
-        <div className="two">
-          <label htmlFor="course_type">Course Type</label>
-          <select name="course_type" placeholder="Choose a course" value={this.state.course_type} onChange={this.handleChange} >
-            <option value="" hidden>Select a course</option>
-            <option value="wdi">Web Development </option>
-            <option value="uxdi">User Expierence</option>
-            <option value="dsi">Data Science</option>
-          </select>
-        </div>
-        <div className="three">
-          <label htmlFor="start_date">Start Date</label>
-          <input type="date" name="start_date" value={this.state.start_date} onChange={this.handleChange} ></input>
-          <br/><button className="submitButton"type="submit">Submit</button>
-        </div>
-        <div className="four">
-          <label htmlFor="end_date" placeholder="yyyy-mm-dd">End Date</label>
-          <input type="date" name="end_date" value={this.state.end_date} onChange={this.handleChange} ></input>
-        </div>
-      </form>
       </div>
-    </div>
-   );
+    );
   }
 }
 
@@ -75,14 +108,15 @@ const mapStatetoProps = state => {
     isAuthenticating: state.isAuthenticating,
     currentUser: state.currentUser,
     errors: state.errors
-  }
-}
+  };
+};
 
 const mapDispatchtoProps = dispatch => {
-  return  {
-    onTokenReceive: token => dispatch({ type: "SET_USER_TOKEN", payload: token})
-  }
-}
+  return {
+    onTokenReceive: token =>
+      dispatch({ type: "SET_USER_TOKEN", payload: token })
+  };
+};
 
 export default connect(
   mapStatetoProps,
